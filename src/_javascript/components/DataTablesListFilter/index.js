e3b9918f24386129a12a1c41afe9e3fdfbbee7d2
Marcos Abreu <marcos.abreu.oliveira@gmail.com>
2020-10-26 11:16:33 -0300

    refactor: portuguese datatables

--- src/_javascript/components/DataTablesListFilter/index.js
+++ src/_javascript/components/DataTablesListFilter/index.js
@@ -1,28 +1,40 @@
 class DataTablesListFilter {
   static bind() {
+    const messages = {
+      search: "",
+      searchPlaceholder: "Procurar paciente",
+      info: "Mostrando de _START_ até _END_ de _TOTAL_ registros",
+      emptyTable: "Nenhum dado disponível na tabela",
+      infoEmpty: "Mostrando de 0 até 0 de 0 registros",
+      infoFiltered: "(Filtrado de _MAX_ registros)",
+      thousands: ".",
+      lengthMenu: "Mostrando _MENU_ registros",
+      loadingRecords: "Carregando...",
+      paginate: {
+        first: "Primeiro",
+        last: "Último",
+        next: "Próximo",
+        previous: "Voltar",
+      },
+    };
+
     $("#pacientes").dataTable({
       language: {
-        search: "",
-        searchPlaceholder: "Procurar paciente",
-        info: "Mostrando de _START_ até _END_ de _TOTAL_ registros",
+        ...messages,
       },
       dom: ' <"top"f>rt<"bottom"ip><"clear">',
     });
 
     $("#cuidadores").dataTable({
       language: {
-        search: "",
-        searchPlaceholder: "Procurar cuidador",
-        info: "Mostrando de _START_ até _END_ de _TOTAL_ registros",
+        ...messages,
       },
       dom: ' <"top"f>rt<"bottom"ip><"clear">',
     });
 
     $("#chamadas").dataTable({
       language: {
-        search: "",
-        searchPlaceholder: "Procurar chamada",
-        info: "Mostrando de _START_ até _END_ de _TOTAL_ registros",
+        ...messages,
       },
       dom: ' <"top"f>rt<"bottom"ip><"clear">',
     });
